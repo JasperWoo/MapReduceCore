637ba6bfbf0333efc5cc9eaf62829245f16f114c
JasperWoo <jasperwoo516@gmail.com>
2017-11-25 09:21:52 -0800

    implemented file sharding and config file reading

--- src/file_shard.h
+++ src/file_shard.h
@@ -1,17 +1,95 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <climits>
+#include <math.h>
 #include "mapreduce_spec.h"
 
+using namespace std;
 
 /* CS6210_TASK: Create your own data structure here, where you can hold information about file splits,
      that your master would use for its own bookkeeping and to convey the tasks to the workers for mapping */
 struct FileShard {
-
+	// string being the file path, pair contains begin position and end position of the file portion.
+	unordered_map<string, pair<streampos, streampos>> files;
 };
+
+streamsize get_file_size(const string& filename) {
+    ifstream fin(filename, ios::ate | ios::binary);
+    streamsize size =  fin.tellg(); 
+    fin.close();
+    return size;
+}
 
 
 /* CS6210_TASK: Create fileshards from the list of input files, map_kilobytes etc. using mr_spec you populated  */ 
 inline bool shard_files(const MapReduceSpec& mr_spec, std::vector<FileShard>& fileShards) {
+	int map_size = mr_spec.map_size;
+	vector<string> filepaths = mr_spec.input_files;
+	streamsize totalsize = 0;
+	for (auto& filepath : filepaths) {
+		totalsize += get_file_size(filepath);
+	}
+	int n_shards = (int)ceil(totalsize / 1024.0 / map_size);
+	fileShards.reserve(n_shards);
+
+	// Actual sharding. For each input file, sha
+	for (auto& filepath : filepaths) {
+		streamsize filesize = get_file_size(filepath);
+		cout << "input file size:" << filesize / 1024.0 << endl;
+		streamsize remain = filesize;
+		// streampos offset = 0;
+		ifstream fin(filepath, ios::binary);
+		while (remain > 0) {
+			streamsize extract_size = map_size;
+			FileShard cur; 
+			if (!fileShards.empty()) {
+				FileShard last_shard = fileShards.back();
+				totalsize = 0;
+				cout <<endl<<endl<< "Entered key point 111" <<endl<<endl<<endl;
+				for (auto it = last_shard.files.begin(); it != last_shard.files.end(); ++it) {
+					totalsize += it->second.second - it->second.first;
+				}
+				cout <<endl<<endl<< "Totalsize:" << totalsize<< endl<<endl<<endl;
+				
+				if (totalsize / 1024.0 < map_size) {
+					// the last shard is not yet finished
+					extract_size = map_size - floor(totalsize / 1024.0);
+					cout <<endl<<endl<< "Extract_size updated to " << extract_size <<endl<<endl<<endl;
+					fileShards.pop_back();
+					cur = last_shard;
+					cout <<endl<<endl<< "Entered key point 222" <<endl<<endl<<endl;
+				}
+			}
+			streampos start = fin.tellg();
+			cout << "tell_get of start : " << start / 1024.0 << endl;
+			// in c++11, seek is able to seek beyond the eof.
+			fin.seekg(extract_size * 1024, ios::cur);
+			streampos end = fin.tellg();
+			cout << "tell_get of end : " << end / 1024.0 << endl;
+			
+			if (end > filesize) {
+				fin.seekg(0, ios::end);
+				end = fin.tellg();
+				remain -= (end - start + 1);
+			} else {
+				fin.ignore(LONG_MAX, '\n');
+				// seek back to last character in the prev line.
+				fin.seekg(-2, ios::cur);
+				end = fin.tellg();
+				// include the newline character.
+				remain -= (end - start + 2);
+				// seek to new line
+				fin.seekg(2, ios::cur);
+			}
+			cout << "tell_get of end after adjustment : " << end / 1024.0 << endl;
+			cur.files[filepath] = make_pair(start, end);
+			fileShards.push_back(cur);
+		}
+		fin.close();
+		cout << endl<<endl<<endl;
+	}
 	return true;
 }
--- src/mapreduce_spec.h
+++ src/mapreduce_spec.h
@@ -1,21 +1,131 @@
 #pragma once
 
 #include <string>
-
-
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <utility>
+using namespace std;
 /* CS6210_TASK: Create your data structure here for storing spec from the config file */
 struct MapReduceSpec {
-
+	int n_workers;     // # of workers
+	int n_outputs; // # of output files
+	int map_size; // size (in kB) of map task input	
+	string output_dir; // output directory
+	string user_id; // identifier of task
+	vector<string> worker_addrs; // worker ip addresses and ports
+	vector<string> input_files;
 };
 
 
 /* CS6210_TASK: Populate MapReduceSpec data structure with the specification from the config file */
-inline bool read_mr_spec_from_config_file(const std::string& config_filename, MapReduceSpec& mr_spec) {
-	return true;
+inline bool read_mr_spec_from_config_file(const string& config_filename, MapReduceSpec& mr_spec) {
+	unordered_map<string, vector<string>> values;
+	ifstream fin(config_filename);
+	if (!fin.fail()) {
+		try {
+			string line;
+			while (getline(fin, line)) {
+				istringstream is_line(line);
+				string key;
+				if (getline(is_line, key, '=')) {
+					string value;
+					if (!getline(is_line, value)) break;
+					vector<string> records;
+					istringstream is_value(value);
+					while (is_value) {
+						string record;
+						if (!getline(is_value, record, ',')) break;
+						records.push_back(record);
+					}
+					values[key] = records;
+				}
+			}
+		} catch (exception& e) {
+			cerr << "[ERROR] : " << e.what() << "Failed parsing config file." << std::endl;
+		}
+		
+		fin.close();
+		try {
+			mr_spec.n_workers = stoi(values["n_workers"][0]);
+			mr_spec.n_outputs = stoi(values["n_output_files"][0]);
+			mr_spec.map_size = stoi(values["map_kilobytes"][0]);
+		} catch(exception& e) {
+			cerr << "[ERROR] in Spec: " << e.what() << " failed to parse integer" << endl;
+			return false;
+		}
+		mr_spec.output_dir = values["output_dir"][0];
+		mr_spec.user_id = values["user_id"][0];
+		// TODO: use move() for the following two.
+		mr_spec.worker_addrs = values["worker_ipaddr_ports"];
+		mr_spec.input_files = values["input_files"];
+		return true;
+	} else {
+		return false;
+	}
 }
 
 
 /* CS6210_TASK: validate the specification read from the config file */
 inline bool validate_mr_spec(const MapReduceSpec& mr_spec) {
-	return true;
+	// validate appropriate number of ip_addr_ports.
+	if (mr_spec.n_workers <= 0 || mr_spec.n_workers != mr_spec.worker_addrs.size()) {
+		cerr << "[ERROR] in Spec: invalid number of workers." << endl;
+		return false;
+	}
+	if (mr_spec.n_outputs <= 0) {
+		cerr << "[ERROR] in Spec: invalid number of output files." << endl;
+		return false;
+	}
+	if (mr_spec.map_size <= 0) {
+		cerr << "[ERROR] in Spec: invalid map size." << endl;
+		return false;
+	}
+	if (mr_spec.output_dir.c_str() == nullptr) {
+		cerr << "[ERROR] in Spec: output directory undefined." << endl;
+		return false;
+	}
+	if (mr_spec.user_id.c_str() == nullptr) {
+		cerr << "[ERROR] in Spec: user id undefined." << endl;
+		return false;
+	}
+	// validate input file path
+	for (auto& filepath : mr_spec.input_files) {
+		ifstream fin(filepath);
+		if (fin.fail()) {
+			cerr << "[ERROR] in Spec: file " << filepath << " does not exist!" << endl;
+			return false;
+		} 
+	}
+	// validate worker address ports
+	for (auto& worker_addr : mr_spec.worker_addrs) {
+		istringstream is_addr(worker_addr);
+		string hostname;
+		if (getline(is_addr, hostname, ':')) {
+			string port;
+			if (hostname.compare("localhost") == 0 && getline(is_addr, port)) {
+				int port_number;
+				try {
+					port_number = stoi(port);
+				} catch (exception& e) {
+					cerr << "[ERROR] in Spec: " << e.what() << " failed to parse port number" << endl;
+					return false; 
+				}
+				if (port_number < 0 || port_number > 65535) {
+					cerr << "[ERROR] in Spec: invalid port number" << endl;
+					return false;
+				} 
+			} else {
+				cerr << "[ERROR] in Spec: invalid host name" << endl;
+				return false;
+			}
+		} else {
+			cerr << "[ERROR] in Spec: invalid worker address" << endl;
+			return false;
+		}
+	}
+
+	return true; // all good!
 }
